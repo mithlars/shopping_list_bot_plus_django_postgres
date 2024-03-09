a538b6a6b32e9ad95e7d2f8d6002b8103504deda
MithLars <a@mith.pro>
2024-03-09 14:38:30 +0400

    djDocker/script.ps1   changed creating docker network row (172.18.0.0)

--- script.ps1
+++ script.ps1
@@ -4,16 +4,17 @@ $DJANGO_USERNAME = [Environment]::GetEnvironmentVariable("DJANGO_USERNAME")
 $DJANGO_USER_PASSWORD = [Environment]::GetEnvironmentVariable("DJANGO_USER_PASSWORD")
 $API_TOKEN = [Environment]::GetEnvironmentVariable("API_TOKEN")
 
-$sshCommand = @"
+
+ssh -i C:\Users\lars5\.ssh\id_rsa root@165.227.159.101 @"
 #!/bin/bash
 export POSTGRES_PASSWORD=$POSTGRES_PASSWORD
 export DJANGO_USERNAME=$DJANGO_USERNAME
 export DJANGO_USER_PASSWORD=$DJANGO_USER_PASSWORD
 export API_TOKEN=$API_TOKEN
+export COMPOSE_PROJECT_NAME=shopping_list
 cd /home/django_bot
+docker network create --subnet=172.18.0.0/16 shopping_list_net 
 git clone https://github.com/mithlars/shopping_list_bot_plus_django_postgres
 cd shopping_list_bot_plus_django_postgres
 docker-compose up -d
 "@
-
-ssh -i C:\Users\lars5\.ssh\id_rsa root@165.227.159.101 "$sshCommand"
