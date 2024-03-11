0322673a19841553da7f0e4ec927fc9a38e44321
MithLars <a@mith.pro>
2024-03-11 15:35:20 +0400

    script.ps1     Automated checking if docker-network exists
        Automated rm old version of code

--- script.ps1
+++ script.ps1
@@ -7,14 +7,29 @@ $API_TOKEN = [Environment]::GetEnvironmentVariable("API_TOKEN")
 
 ssh -i C:\Users\lars5\.ssh\id_rsa root@165.227.159.101 @"
 #!/bin/bash
+# Экспорт переменных в sh-сессию:
 export POSTGRES_PASSWORD=$POSTGRES_PASSWORD
 export DJANGO_USERNAME=$DJANGO_USERNAME
 export DJANGO_USER_PASSWORD=$DJANGO_USER_PASSWORD
 export API_TOKEN=$API_TOKEN
 export COMPOSE_PROJECT_NAME=shopping_list
+
+# Проверяем существование docker-сети:
+docker network inspect shopping_list_net >/dev/null 2>&1 && \
+# Если сеть существует, проверяем настройки подсети:
+SUBNET=$(docker network inspect shopping_list_net -f '{{(index .IPAM.Config 0).Subnet}}') || \
+# Если сеть не существует, создаем новую:
+docker network create --subnet=172.18.0.0/16 shopping_list_net
+# Если сеть существует, но настройки подсети отличаются, создаем новую:
+if [ "$SUBNET" != "172.18.0.0/16" ]; then
+  docker network rm shopping_list_net
+  docker network create --subnet=172.18.0.0/16 shopping_list_net
+fi
+
 cd /home/django_bot
-docker network create --subnet=172.18.0.0/16 shopping_list_net 
+rm -rf ./*
 git clone https://github.com/mithlars/shopping_list_bot_plus_django_postgres
+
 cd shopping_list_bot_plus_django_postgres
 docker-compose up -d
 "@
