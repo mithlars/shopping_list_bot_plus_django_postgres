88754fe12e6acdf06c4bb128ea15b38b613e9a6e
MithLars <a@mith.pro>
2024-03-20 21:24:01 +0400

    Replace purchase read interface to archive, because main purchase interface now is purchase delete interface. Replaced code from purchase delete file to purchase delete and list menu file

--- bot/business_processes/purchases/purchases_delete_and_list_menu.py
+++ bot/business_processes/purchases/purchases_delete_and_list_menu.py
@@ -1,7 +1,7 @@
 from typing import Dict, Any, Tuple
 
 from aiogram import types, Router, F
-from aiogram.types import InlineKeyboardButton, KeyboardButton, InlineKeyboardMarkup
+from aiogram.types import InlineKeyboardButton, KeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 
 from bot.business_processes.lists.utils.lists_menu_keyboard import lists_menu_keyboard_buttons
@@ -12,11 +12,15 @@ from bot.business_processes.purchases.utils.list_menu_keyboard import list_menu_
 from bot.constants import django_address, buttons_styles
 from bot.create_bot import MyBot
 from bot.api.django_auth import django_auth, update_last_request_time
-from bot.emoji import emoji
 from bot.translate import transl
 
+from aiogram.utils.i18n import gettext as _
 
-class ListDataProcessing:
+
+purchases_delete_and_list_menu = Router()
+
+
+class ListRead:
 
     @staticmethod
     async def data_to_text_and_keyboard(list_data: list) -> Tuple[str, InlineKeyboardMarkup]:
@@ -38,12 +42,9 @@ class ListDataProcessing:
                                                      callback_data=f"del_pur {purchase_dict['id']}"))
                     number_count += 1
         if message_text == "":
-            message_text = "Список (категории группы) пока пуст(ы)."
+            message_text = _("List (group) is steel empty")
         keyboard = builder.as_markup(resize_keyboard=True)
         return message_text, keyboard
-
-
-class ListReadAPI:
 
     @staticmethod
     @update_last_request_time(django_auth)
@@ -52,14 +53,8 @@ class ListReadAPI:
         url = f"{django_address}/lists/list_or_group/"
         data = {"telegram_user_id": telegram_user_id}
         response = django_auth.session.get(url=url, data=data)
-        message_text, keyboard = await ListDataProcessing.data_to_text_and_keyboard(response.json())
+        message_text, keyboard = await ListRead.data_to_text_and_keyboard(response.json())
         return message_text, keyboard
-
-
-list_read_router = Router()
-
-
-class ListRead:
 
     @staticmethod
     async def get_current_lists_purchases_list(telegram_user_id: int):
@@ -69,7 +64,7 @@ class ListRead:
         """
         # Запрос возвращает имя списка для сообщения:
         list_name = await get_lists_detail_api(telegram_user_id)
-        list_message_text = f"Список \"{list_name}\":"
+        list_message_text = _("List \"{list_name}\"").format(list_name=list_name)
 
         # Отправка сообщения "Список {list_name}:" с выводом основной клавиатуры:
         await MyBot.bot.send_message(chat_id=telegram_user_id,
@@ -77,7 +72,7 @@ class ListRead:
                                      reply_markup=await list_menu_keyboard_builder(telegram_user_id))
 
         # Запрос возвращает текст сообщения с содержимым списка или группы и клавиатуру удаления позиций:
-        message_text, keyboard = await ListReadAPI.get_hole_list(telegram_user_id)
+        message_text, keyboard = await ListRead.get_hole_list(telegram_user_id)
 
         # Отправка сообщения с содержимым списка и inline-клавиатурой для удаления позиций:
         await MyBot.bot.send_message(chat_id=telegram_user_id,
@@ -85,7 +80,7 @@ class ListRead:
                                      reply_markup=keyboard)
 
     @staticmethod
-    @list_read_router.message(
+    @purchases_delete_and_list_menu.message(
         lambda message:
         any(message.text == list_menu_keyboard_buttons(lang)[button_style]['reload']
             for lang in transl.keys() for button_style in buttons_styles)
@@ -97,3 +92,25 @@ class ListRead:
     async def list_of_purchases_handler(message: types.Message):
         """ Handler-функция для вывода списка с клавиатурой для удаления позиций """
         await ListRead.get_current_lists_purchases_list(message.from_user.id)
+
+
+class PurchasesDelete:
+
+    @staticmethod
+    @update_last_request_time(django_auth)
+    async def delete_purchase_api(telegram_user_id: int, purchase_id: int):
+        url = f"{django_address}/purchases/detail/"
+        data = {
+            "telegram_user_id": telegram_user_id,
+            "purchase_id": purchase_id
+        }
+        await django_auth.session.delete(url=url, data=data)
+
+    @staticmethod
+    @purchases_delete_and_list_menu.callback_query(lambda c: c.data and c.data.startswith('del_pur'))
+    async def delete_purchase(callback: CallbackQuery):
+        data = callback.data.split(" ")
+        purchase_id = int(data[1])
+        telegram_user_id = callback.from_user.id
+        await PurchasesDelete.delete_purchase_api(telegram_user_id, purchase_id)
+        await ListRead.get_current_lists_purchases_list(telegram_user_id)
