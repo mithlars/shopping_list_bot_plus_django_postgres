1c09081516dfb41cb72697edee408d654068e04f
MithLars <a@mith.pro>
2024-04-20 16:33:58 +0400

    adit allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-@p1b1w!a0rlnrx9z65wt80ur&y$hjl-jgdr($47i7+1dt5uxxl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True  # TODO: зафиксировать в env и сделать разный для продакшена
 
-ALLOWED_HOSTS = ['django', '127.0.0.1', '165.227.159.101', '172.17.2.3', '172.18.2.3']
+ALLOWED_HOSTS = ['django', '127.0.0.1', '165.227.159.101', '172.16.0.3', '172.19.0.3']
 
 
 # Application definition
